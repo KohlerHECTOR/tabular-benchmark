d9039525ab8f6823e0c5274818428ab5bee66bd8
KohlerHECTOR <hector.kohler@inria.fr>
2024-12-12 11:46:55 +0100

    equalize neites on numerical benchmak all boosting

--- analyses/benchmark_plots_numerical_classif_boosting_all.R
+++ analyses/benchmark_plots_numerical_classif_boosting_all.R
@@ -21,11 +21,11 @@ df <- benchmark %>%
 df <- rename(df)
 checks(df)
 
-plot_results_per_dataset(df, "accuracy", default_colscale = T, equalize_n_iteration = F, max_iter = 100)
+plot_results_per_dataset(df, "accuracy", default_colscale = T, equalize_n_iteration = T, max_iter = 100)
 ggsave("analyses/plots/random_search_classif_numerical_datasets_boosting_all.pdf", width=15, height=10, bg="white")
 
 
 # Aggregated
 
-plot_aggregated_results(df, y_inf=0.7, y_sup=1, score="accuracy", quantile=0.1, truncate_scores = F, max_iter = 100, equalize_n_iteration = F)
+plot_aggregated_results(df, y_inf=0.7, y_sup=1, score="accuracy", quantile=0.1, truncate_scores = F, max_iter = 100, equalize_n_iteration = T)
 ggsave("analyses/plots/random_search_classif_numerical_boosting_all.pdf", width=7, height=6, bg="white")
